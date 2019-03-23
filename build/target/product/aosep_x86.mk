cd67fa3d2835639d8b8919ff2b6365eeda60fecd
arshaprasad <42238183+arshaprasad@users.noreply.github.com>
2019-03-23 11:46:24 +0530

    Update and rename validus_x86.mk to aosep_x86.mk

--- build/target/product/aosep_x86.mk
+++ build/target/product/aosep_x86.mk
@@ -13,11 +13,11 @@
 # limitations under the License.
 
 $(call inherit-product, build/target/product/aosp_x86.mk)
-$(call inherit-product, vendor/validus/config/common.mk)
+$(call inherit-product, vendor/aosep/config/common.mk)
 
 # Allow building otatools
 TARGET_FORCE_OTA_PACKAGE := true
 
 TARGET_USES_64_BIT_BINDER := true
 
-PRODUCT_NAME := validus_x86
+PRODUCT_NAME := aosep_x86
