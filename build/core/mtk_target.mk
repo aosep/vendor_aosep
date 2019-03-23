441610eb3939574e68002d87f4bdb05e792d414d
ameerava <42238138+ameerava@users.noreply.github.com>
2019-03-23 11:26:05 +0530

    Update mtk_target.mk

--- build/core/mtk_target.mk
+++ build/core/mtk_target.mk
@@ -1,5 +1,5 @@
 # Bring in Mediatek helper macros
-include vendor/validus/build/core/mtk_utils.mk
+include vendor/aosep/build/core/mtk_utils.mk
 
 ifeq ($(BOARD_USES_MTK_HARDWARE),true)
     mtk_flags := -DMTK_HARDWARE
