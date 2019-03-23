814857f8f2e284a93051598e5dc231d346b3ae3d
ameerava <42238138+ameerava@users.noreply.github.com>
2019-03-23 15:08:02 +0530

    Update config.mk

--- build/core/config.mk
+++ build/core/config.mk
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/validus/build/core/system_rro.mk
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/aosep/build/core/system_rro.mk
 SELINUX_IGNORE_NEVERALLOWS := true
