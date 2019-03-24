c6ea02e05e036b753cf683eed18d66e59297f398
Anis Noorudheen <35500897+anisnoorudheen@users.noreply.github.com>
2019-03-24 05:37:44 +0530

    Update variables.go

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func gzospExpandVariables(ctx android.ModuleContext, in string) string {
-	gzospVars := ctx.Config().VendorConfig("gzospVarsPlugin")
+func aosepExpandVariables(ctx android.ModuleContext, in string) string {
+	 := ctx.Config().VendorConfig("aosepVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if gzospVars.IsSet(name) {
-			return gzospVars.String(name), nil
+		if aosepVars.IsSet(name) {
+			return aosepVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
